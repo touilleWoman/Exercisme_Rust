579c127ccd2b2150d1e2eabcd943ce41410f68f3
touilleWoman <jie.zhou.ac@gmail.com>
2021-04-11 21:58:32 +0200

    modif reverse-string to get bonus

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,11 +1,8 @@
+//do "hexdump -C tests/reverse-string.rs" to look at binary of "uüu" in the test
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    let ret: String = input.chars().rev().collect();
-    return ret
+    input.graphemes(true).rev().collect()
 }
 
-pub fn main(){
-    let input : &str = "uüu";
-    let ret = reverse(input);
-    println!("{}", ret);
-}
 
