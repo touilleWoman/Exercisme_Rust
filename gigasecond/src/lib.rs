0a4347ee5fb7aa8c9d0913b8007864d4c9d46c32
touilleWoman <jie.zhou.ac@gmail.com>
2021-07-29 16:27:48 +0200

    Exercise gigasecond done

---
+++ gigasecond/src/lib.rs
@@ -0,0 +1,16 @@
+use chrono::{DateTime, Duration, Utc};
+// Returns a Utc DateTime one billion seconds after start.
+
+pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+    const GIGA: i64 = 1_000_000_000;
+    let after = start.checked_add_signed(Duration::seconds(GIGA));
+    match after {
+        Some(x) => {
+            println!("{}", x);
+            x
+        }
+        None => {
+            panic!("Overflow");
+        }
+    }
+}
