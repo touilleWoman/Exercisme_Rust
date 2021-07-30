12f28a4db8e11136a7ad9cc965c75b29a17dd317
touilleWoman <jie.zhou.ac@gmail.com>
2021-07-30 13:01:48 +0200

    refactor(gigasecond): add a second solution

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -14,3 +14,7 @@ pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
         }
     }
 }
+
+// pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+//     start + Duration::seconds(1_000_000_000)
+// }
