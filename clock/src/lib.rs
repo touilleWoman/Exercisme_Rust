9079631411c5f1c482812cf8f9d9278e25719028
touilleWoman <jie.zhou.ac@gmail.com>
2021-08-18 15:16:50 +0200

    refactor(clock): use NaiveTime as struct content

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,37 +1,31 @@
+use chrono::{Duration, NaiveTime};
 use std::fmt;
-
-use chrono::{Duration, NaiveTime, Timelike};
 
 #[derive(Debug, PartialEq)]
 pub struct Clock {
-    hours: i32,
-    minutes: i32,
-}
-
-impl fmt::Display for Clock {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
-    }
+    time: NaiveTime,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let time = NaiveTime::from_hms(0, 0, 0);
-        let (time, _nb) = time.overflowing_add_signed(Duration::minutes(minutes.into()));
-        let (time, _nb) = time.overflowing_add_signed(Duration::hours(hours.into()));
-
         Clock {
-            hours: time.hour() as i32,
-            minutes: time.minute() as i32,
+            time: NaiveTime::from_hms(0, 0, 0)
+                + Duration::hours(hours as i64)
+                + Duration::minutes(minutes as i64),
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let time = NaiveTime::from_hms(self.hours as u32, self.minutes as u32, 0);
-        let (time, _nb) = time.overflowing_add_signed(Duration::minutes(minutes.into()));
         Clock {
-            hours: time.hour() as i32,
-            minutes: time.minute() as i32,
+            time: self.time + Duration::minutes(minutes as i64),
         }
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let formatted_time = self.time.format("%H:%M").to_string();
+
+        write!(f, "{}", formatted_time)
     }
 }
